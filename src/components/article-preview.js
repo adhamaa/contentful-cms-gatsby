059c33939e068ddadbc1d11d1ed7e602eb90ba10
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-07-09 18:47:28 +0800

    add tags and setup contentful API

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react";
+import { Link } from "gatsby";
+import Img from "gatsby-image";
 
-import styles from './article-preview.module.css'
+import styles from "./article-preview.module.css";
 
 export default ({ article }) => (
   <div className={styles.preview}>
@@ -11,10 +11,16 @@ export default ({ article }) => (
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
     <small>{article.publishDate}</small>
-    <p
+    <div
       dangerouslySetInnerHTML={{
         __html: article.description.childMarkdownRemark.html,
       }}
     />
+    {article.tags &&
+      article.tags.map((tag) => (
+        <p className={styles.tag} key={tag}>
+          {tag}
+        </p>
+      ))}
   </div>
-)
+);
